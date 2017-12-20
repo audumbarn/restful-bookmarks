6a44c82215459cd3e60c4b50227b37aa3073eb3c
Audumbar Nevarekar <audumbar.raj.nevarekar@dxc.com>
2017-12-20 13:25:09 +0530

    initial commit

---
+++ src/main/java/com/projects/bookmarks/RestfulBookmarksApplication.java
@@ -0,0 +1,12 @@
+package com.projects.bookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulBookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulBookmarksApplication.class, args);
+	}
+}
